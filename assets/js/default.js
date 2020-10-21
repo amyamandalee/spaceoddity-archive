b9fa097f73041c3cd57badca733a09d9c3bb2bce
amyamandalee <69936564+amyamandalee@users.noreply.github.com>
2020-10-22 11:12:24 +1300

    Add files via upload
    latest js

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,4 +4,9 @@ $(function() {
     $(".so-bounding-box").toggleClass("so-bounding-box--show-guitar");
     $(".so-body").toggleClass("so-body--cursor--no-guitar");
   });
+  $('body').on('click', '#toggle-overlay', function(e) {
+    e.preventDefault();
+    $(".so-overlay-page").toggleClass("so-overlay-page--open");
+    $(".so-button").toggleClass("so-button--open");
+  });
 });
